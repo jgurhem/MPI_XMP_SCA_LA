deca6f6b499503b735878db6693f10025c0435e1
Jérôme Gurhem <jerome.gurhem@polytech-lille.net>
2018-10-20 19:00:58 +0200

    Fix environment problem preventing code from functionning

--- scripts/load.sh
+++ scripts/load.sh
@@ -1,4 +1,4 @@
 module purge
 module load gnu/4.7.2 gnu-env/4.7.2 intel intelmpi autotools/Feb2014 lapack/3.5_gnu47
 #module load openblas/v0.2.13_gnu47 
-#export I_MPI_DAPL_PROVIDER=ofa-v2-ib0
+export I_MPI_DAPL_PROVIDER=ofa-v2-ib0
